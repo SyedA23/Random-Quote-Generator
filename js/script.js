993aa2067be356d9c8be3dc4ed8b1138e05a5da0
Areeb <areebahmad613@gmail.com>
2025-02-18 20:09:31 +0530

    Fixed bugs in code

--- js/script.js
+++ js/script.js
@@ -44,14 +44,19 @@ let quotes = [
   }
 ];
 // Store the array in Local Storage
-localStorage.setItem("quotes", JSON.stringify(quotes));
+// localStorage.setItem("quotes", JSON.stringify(quotes));
 
 // Retrieve the array and parse it back to JavaScript objects
-let storedQuotes = JSON.parse(localStorage.getItem("quotes"));
+let storedQuotes = JSON.parse(localStorage.getItem("quotes")) || [] ;
+console.log("lenght is",storedQuotes.length);
 
 // Function to get a random quote
 function getRandomQuote() {
   const randomIndex = Math.floor(Math.random() * storedQuotes.length);
+  console.log("lenght is",storedQuotes.length);
+  
+  console.log("random index is", randomIndex);
+
   const randomQuote = storedQuotes[randomIndex];
 
   // Update the quote text and author in the HTML
@@ -63,14 +68,14 @@ document
   .querySelector(".generate-button")
   .addEventListener("click", getRandomQuote);
 
-function getQuotes() {
-  let storedQuotes = localStorage.getItem("quotes"); // Get data from localStorage
-  if (storedQuotes) {
-    return JSON.parse(storedQuotes); // Convert JSON string to an array
-  } else {
-    return []; // Return an empty array if no quotes are found
-  }
-}
+// function getQuotes() {
+//   let storedQuotes = localStorage.getItem("quotes"); // Get data from localStorage
+//   if (storedQuotes) {
+//     return JSON.parse(storedQuotes); // Convert JSON string to an array
+//   } else {
+//     return []; // Return an empty array if no quotes are found
+//   }
+// }
 
 // Function to add a new quote
 function addNewQuote() {
@@ -82,11 +87,13 @@ function addNewQuote() {
     return;
   }
 
-  let quotes = getQuotes(); // Get existing quotes
-  let newQuote = { text: quoteText, author: quoteAuthor }; // Create a new quote object
-  quotes.push(newQuote); // Add new quote to the array
+  // let quotes = getQuotes(); // Get existing quotes
+  let newQuote = { text: quoteText, author: quoteAuthor };
+  console.log(newQuote);
+   // Create a new quote object
+  storedQuotes.push(newQuote); // Add new quote to the array
 
-  localStorage.setItem("quotes", JSON.stringify(quotes)); // Store updated quotes
+  localStorage.setItem("quotes", JSON.stringify(storedQuotes)); // Store updated quotes
 
   // Clear input fields after adding a quote
   document.getElementById("new-quote").value = "";
